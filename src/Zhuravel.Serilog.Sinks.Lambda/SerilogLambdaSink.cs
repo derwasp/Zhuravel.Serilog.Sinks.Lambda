6979e0a9112726c743b90321502f012ab597977b
derwasp <nbcwasp@gmail.com>
2017-08-14 10:00:05 +0000

    initial solution

---
+++ src/Zhuravel.Serilog.Sinks.Lambda/SerilogLambdaSink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+using Amazon.Lambda.Core;
+
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace Zhuravel.Serilog.Sinks.Lambda
+{
+    internal sealed class SerilogLambdaSink : ILogEventSink
+    {
+        readonly ITextFormatter _textFormatter;
+
+        public SerilogLambdaSink(ITextFormatter textFormatter)
+        {
+            _textFormatter = textFormatter ?? throw new ArgumentNullException(nameof(textFormatter));
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            if (logEvent == null)
+            {
+                throw new ArgumentNullException(nameof(logEvent));
+            }
+
+            using (var renderSpace = new StringWriter())
+            {
+                _textFormatter.Format(logEvent, renderSpace);
+                LambdaLogger.Log(renderSpace.ToString());
+            }
+        }
+    }
+}
---
+++ src/Zhuravel.Serilog.Sinks.Lambda/SerilogLambdaSinkConfigurationExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+
+namespace Zhuravel.Serilog.Sinks.Lambda
+{
+    public static class SerilogLambdaSinkConfigurationExtensions
+    {
+        const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}";
+
+        /// <summary>
+        /// Writes log events to AWS Lambda CloudWatch
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for
+        /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level
+        /// to be changed at runtime.</param>
+        /// <param name="outputTemplate">A message template describing the format used to write to the sink.
+        /// the default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration CloudwatchLambda(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            string outputTemplate = DefaultOutputTemplate,
+            IFormatProvider formatProvider = null,
+            LoggingLevelSwitch levelSwitch = null)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+            if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+            return CloudwatchLambda(sinkConfiguration, formatter, restrictedToMinimumLevel, levelSwitch);
+        }
+
+        /// <summary>
+        /// Writes log events to AWS Lambda CloudWatch
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="formatter">Controls the rendering of log events into text, for example to log JSON. To
+        /// control plain text formatting, use the overload that accepts an output template.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for
+        /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level
+        /// to be changed at runtime.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration CloudwatchLambda(
+            this LoggerSinkConfiguration sinkConfiguration,
+            ITextFormatter formatter,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            LoggingLevelSwitch levelSwitch = null)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+            if (formatter == null) throw new ArgumentNullException(nameof(formatter));
+            return sinkConfiguration.Sink(new SerilogLambdaSink(formatter), restrictedToMinimumLevel, levelSwitch);
+        }
+    }
+}
